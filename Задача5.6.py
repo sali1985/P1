31d99189b7fe3c4cc60e823d7f0e26c67d4367ad
geekbrains <info@geekbrains.ru>
2020-09-11 15:46:09 +0300

    Задача5.6

---
+++ Задача5.6.py
@@ -0,0 +1,7 @@
+
+subj = {}
+with open('work', 'r', encoding = 'utf - 8') as f:
+    for line in f:
+        subject, lecture, practice, lab = line.split()
+        subj[subject] = int(lecture) + int(practice) + int(lab)
+    print(f'Общее количество часов по предмету - \n {subj}')
